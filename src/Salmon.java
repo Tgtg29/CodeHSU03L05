76bf278b1ce50b70f2ceb4fe7521465fa7ffab3d
Tgtg29 <tgtg7871@gmail.com>
2022-11-09 09:28:13 -0800

    Completed Lesson

--- src/Salmon.java
+++ src/Salmon.java
@@ -21,7 +21,7 @@ public class Salmon {
         // Ask user for month of year as an integer
         System.out.println("Enter the month of the year as a number: ");
         int month = input.nextInt();
-        // Use if/else if/else statement to determine if it is spawning season
+       /* // Use if/else if/else statement to determine if it is spawning season
         if(month<3){
             System.out.println("Not spawning season");
         }
@@ -38,5 +38,13 @@ public class Salmon {
             System.out.println("Not spawning season");
         }
 
+         */
+        if(month >= 3 && month <= 6){
+            System.out.println("Spring spawning season.");
+        }else if(month >= 9 && month <= 11){
+            System.out.println("Fall spawning season.");
+        }else{
+            System.out.println("Not spawning season.");
+        }
     }
 }
